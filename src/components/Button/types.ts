51fc829f8ad8a75e73e281aad0764e5a9e519367
Swayangprajna <swayangprajna@gmail.com>
2024-10-21 22:16:19 +0530

    upadated files

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,5 +1,5 @@
 export type ButtonProps = {
   type?: "primary" | "secondary" | "ternary";
   disabled?: boolean;
-  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: () => void;
 };
